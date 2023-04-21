4bd1a6def0c22cf4d75d5ffb019876e0df4971e8
0xKitsune <0xKitsune@protonmail.com>
2023-04-21 16:23:12 -0400

    updated note on mevmanager

--- src/state.rs
+++ src/state.rs
@@ -5,17 +5,22 @@ use std::{
 
 use arraydeque::ArrayDeque;
 use damms::amm::{AutomatedMarketMaker, AMM};
-use ethers::{prelude::k256::elliptic_curve::rand_core::block, providers::Middleware, types::H160};
+use ethers::{providers::Middleware, types::H160};
 
 use crate::{
     error::{StateChangeError, StateSpaceError},
     state_change::StateChange,
 };
 
+
+//TODO:FIXME:
+    //mevmanager should take tobstrat closure and txpoolstrat closure, have some sort of logic maintaining the statespace changes
+    //One potential idea is to pass in the tx for the state changes stream or some stream, and then have the state being managed elsewhere not in the mev manager, not sure yet.
+    //also could be internal to the mev manager
 pub type StateSpace = HashMap<H160, AMM>;
 
 pub struct StateSpaceManager<M: Middleware> {
-    pub state: Arc<RwLock<StateSpace>>,
+    pub state: Arc<RwLock<StateSpace>>, //TODO: consider that the state should NEVER while routing is occurring, account for this
     pub last_synced_block: u64,
     pub state_change_cache: ArrayDeque<StateChange, 150>,
     pub middleware: Arc<M>,
@@ -34,15 +39,35 @@ where
         }
     }
 
-    //TODO: return an rx that will allow someone to subscribe and listen for the state changes
 
-    //returns a reciever that signals when a specific amm has changed
-    // pub fn listen_for_state_changes<M:Middleware>(ws provider) -> Result<Receiver<H160>, StateSpaceError<M>> {
+
+    // pub fn get_block_filter(&self){}
+
+    //pub fn handle_state_changes(&mut self, logs: Vec<Log>){}
+    //pub fn handle_state_change(&mut self, log: Log){}
+
+    //listens to new blocks and handles state changes, sending an h256 block hash when a new block is produced
+    //pub fn listen_for_new_blocks()-> Result<Receiver<H256>, StateSpaceError<M>> {}
+
+    //returns a reicever that signals when a specific amm has changed
+    // pub fn listen_for_state_changes<M:Middleware>(ws provider) -> Result<Receiver<Vec<H160>>, StateSpaceError<M>> {
 
     //     Ok(())
     // }
 
-    //fn update_state_from_logs
+    pub fn listen_for_state_changes<M:Middleware>(stream_provider) -> Result<Receiver<Vec<H160>>, StateSpaceError<M>> {
+
+        //sub to new block headers,
+
+        //handle the state changes
+
+        //send all of the affected amm address through the channel
+
+            Ok(())
+        }
+    }
+
+    //update state from logs fn
 
     //Unwinds the state changes cache for every block from the most recent state change cache back to the block to unwind -1
     pub fn unwind_state_changes(&mut self, block_to_unwind: u64) -> Result<(), StateChangeError> {
