573870883e0d3491e290d40eaba3cc60cba53547
0xKitsune <0xKitsune@protonmail.com>
2023-04-22 00:44:47 -0400

    added stream generic

--- src/state.rs
+++ src/state.rs
@@ -1,70 +1,115 @@
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet},
     sync::{mpsc::Receiver, Arc, RwLock},
 };
 
 use arraydeque::ArrayDeque;
 use damms::amm::{AutomatedMarketMaker, AMM};
-use ethers::{providers::Middleware, types::H160};
+use ethers::{
+    providers::{Middleware, PubsubClient},
+    types::{Filter, H160, H256},
+};
 
 use crate::{
     error::{StateChangeError, StateSpaceError},
     state_change::StateChange,
 };
 
-
 //TODO:FIXME:
-    //mevmanager should take tobstrat closure and txpoolstrat closure, have some sort of logic maintaining the statespace changes
-    //One potential idea is to pass in the tx for the state changes stream or some stream, and then have the state being managed elsewhere not in the mev manager, not sure yet.
-    //also could be internal to the mev manager
+//mevmanager should take tobstrat closure and txpoolstrat closure, have some sort of logic maintaining the statespace changes
+//One potential idea is to pass in the tx for the state changes stream or some stream, and then have the state being managed elsewhere not in the mev manager, not sure yet.
+//also could be internal to the mev manager
 pub type StateSpace = HashMap<H160, AMM>;
 
-pub struct StateSpaceManager<M: Middleware> {
+pub struct StateSpaceManager<M: Middleware, S: PubsubClient> {
     pub state: Arc<RwLock<StateSpace>>, //TODO: consider that the state should NEVER while routing is occurring, account for this
     pub last_synced_block: u64,
     pub state_change_cache: ArrayDeque<StateChange, 150>,
     pub middleware: Arc<M>,
+    pub stream_middleware: Arc<S>,
 }
 
-impl<M> StateSpaceManager<M>
+impl<M, S> StateSpaceManager<M, S>
 where
     M: Middleware,
+    S: Middleware + PubsubClient,
 {
-    pub fn new(state: StateSpace, last_synced_block: u64, middleware: Arc<M>) -> Self {
+    pub fn new(
+        state: StateSpace,
+        last_synced_block: u64,
+        middleware: Arc<M>,
+        stream_middleware: Arc<S>,
+    ) -> Self {
         Self {
             state: Arc::new(RwLock::new(state)),
             last_synced_block,
             state_change_cache: ArrayDeque::new(),
             middleware,
+            stream_middleware,
         }
     }
 
+    pub fn get_block_filter(&self) -> Filter {
+        //Create the event log signature
+        let mut event_signatures: Vec<H256> = vec![];
+        let mut amm_variants = HashSet::new();
 
+        for amm in self.state.read().unwrap().values() {
+            let variant = match amm {
+                AMM::UniswapV2Pool(_) => 0,
+                AMM::UniswapV3Pool(_) => 1,
+                AMM::ERC4626Vault(_) => 2,
+            };
 
-    // pub fn get_block_filter(&self){}
+            if !amm_variants.contains(&variant) {
+                amm_variants.insert(variant);
+                event_signatures.extend(amm.sync_on_event_signatures());
+            }
+        }
+
+        //Create a new filter
+        Filter::new().topic0(event_signatures)
+    }
 
     //pub fn handle_state_changes(&mut self, logs: Vec<Log>){}
     //pub fn handle_state_change(&mut self, log: Log){}
 
     //listens to new blocks and handles state changes, sending an h256 block hash when a new block is produced
     //pub fn listen_for_new_blocks()-> Result<Receiver<H256>, StateSpaceError<M>> {}
+    pub fn listen_for_new_blocks() -> Result<Receiver<H256>, StateSpaceError<M>> {
+        let (tx, rx) = std::sync::mpsc::channel();
 
-    //returns a receiver that signals when a specific amm has changed
-    // pub fn listen_for_state_changes<M:Middleware>(ws provider) -> Result<Receiver<Vec<H160>>, StateSpaceError<M>> {
+        tokio::spawn(async move {});
 
-    //     Ok(())
-    // }
+        //TODO: maybe collect handles to await
 
-    pub fn listen_for_state_changes<M:Middleware>(stream_provider) -> Result<Receiver<Vec<H160>>, StateSpaceError<M>> {
-
-        //sub to new block headers,
+        //sub to new block headers
 
         //handle the state changes
 
         //send all of the affected amm address through the channel
 
-            Ok(())
-        }
+        Ok(rx)
+    }
+
+    pub fn listen_for_state_changes(&self) -> Result<Receiver<Vec<H160>>, StateSpaceError<M>>
+    where
+        <S as Middleware>::Provider: PubsubClient,
+    {
+        let (tx, rx) = std::sync::mpsc::channel();
+
+        let block_stream = self.stream_middleware.subscribe_blocks();
+        tokio::spawn(async move {});
+
+        //TODO: maybe collect handles to await
+
+        //sub to new block headers
+
+        //handle the state changes
+
+        //send all of the affected amm address through the channel
+
+        Ok(rx)
     }
 
     //update state from logs fn
